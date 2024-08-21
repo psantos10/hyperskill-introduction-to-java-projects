cb87906d54f86e37cb75a1688a1cd8fc37891dd1
Patrício dos Santos <hello@psantos.dev>
2024-08-21 09:34:23 +0100

    Simple Chat Bot with Java - Stage 2/5: Introduce yourself

--- bot/SimpleBot.java
+++ bot/SimpleBot.java
@@ -1,8 +1,20 @@
 package bot;
 
+import java.util.Scanner;
+
 public class SimpleBot {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! My name is Aid.");
         System.out.println("I was created in 2024.");
+
+        System.out.println("Please, remind me your name.");
+        System.out.print("> ");
+        String name = scanner.nextLine();
+
+        System.out.printf("What a great name you have, %s\n", name);
+
+        scanner.close();
     }
 }
