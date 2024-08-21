24e693e0e9bd4e1555435bc2b7f8b34ba270b26a
Patrício dos Santos <hello@psantos.dev>
2024-08-21 11:49:40 +0100

    Simple Tic-Tac-Toe (Java) - Stage 3/5: What's up on the field

--- tictactoe/Main.java
+++ tictactoe/Main.java
@@ -10,24 +10,83 @@ public class Main {
 }
 
 class Game {
+    private char[][] grid = new char[3][3];
+
     public void start() {
         Scanner scanner = new Scanner(System.in);
 
         String gameState = scanner.nextLine();
-        displayGrid(gameState);
+        setGrid(gameState);
+        displayGrid();
+        analyzeGameState();
 
         scanner.close();
     }
 
-    void displayGrid(String gameState) {
-        var firstLine = gameState.substring(0, 3).toCharArray();
-        var secondLine = gameState.substring(3, 6).toCharArray();
-        var thirdLine = gameState.substring(6).toCharArray();
+    private void displayGrid() {
+        System.out.println("---------");
+        System.out.printf("| %c %c %c |\n", grid[0][0], grid[0][1], grid[0][2]);
+        System.out.printf("| %c %c %c |\n", grid[1][0], grid[1][1], grid[1][2]);
+        System.out.printf("| %c %c %c |\n", grid[2][0], grid[2][1], grid[2][2]);
+        System.out.println("---------");
+    }
 
-        System.out.println("---------");
-        System.out.printf("| %c %c %c |\n", firstLine[0], firstLine[1], firstLine[2]);
-        System.out.printf("| %c %c %c |\n", secondLine[0], secondLine[1], secondLine[2]);
-        System.out.printf("| %c %c %c |\n", thirdLine[0], thirdLine[1], thirdLine[2]);
-        System.out.println("---------");
+    private void setGrid(String gameState) {
+        grid[0] = gameState.substring(0, 3).toCharArray();
+        grid[1] = gameState.substring(3, 6).toCharArray();
+        grid[2] = gameState.substring(6).toCharArray();
+    }
+
+    private boolean checkWinner(char player) {
+        for (int i = 0; i < 3; i++) {
+            if (grid[i][0] == player && grid[i][1] == player && grid[i][2] == player) {
+                return true;
+            }
+
+            if (grid[0][i] == player && grid[1][i] == player && grid[2][i] == player) {
+                return true;
+            }
+        }
+
+        if (grid[0][0] == player && grid[1][1] == player && grid[2][2] == player) {
+            return true;
+        }
+
+        if (grid[0][2] == player && grid[1][1] == player && grid[2][0] == player) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void analyzeGameState() {
+        boolean xWins = checkWinner('X');
+        boolean oWins = checkWinner('O');
+
+        if (xWins && oWins || Math.abs(countChar('X') - countChar('O')) > 1) {
+            System.out.println("Impossible");
+        } else if (xWins) {
+            System.out.println("X wins");
+        } else if (oWins) {
+            System.out.println("O wins");
+        } else if (countChar('_') == 0) {
+            System.out.println("Draw");
+        } else {
+            System.out.println("Game not finished");
+        }
+    }
+
+    private int countChar(char c) {
+        int count = 0;
+
+        for (char[] row : grid) {
+            for (char cell : row) {
+                if (cell == c) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
     }
 }
